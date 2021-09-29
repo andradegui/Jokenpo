0030d0298d337fcb620184ebc5ef799c5cb3d65d
Guilherme Lima <glima@noovi.com.br>
2021-09-29 16:16:37 -0300

    Escolha Jokenpo Player e Computer Finalizados

--- jokenpo.js
+++ jokenpo.js
@@ -1,30 +1,44 @@
 let player = {
-    escolha:"",       
-    buscaEscolhaJokenpo:function(){
+    escolha: "",
+    buscaEscolhaJokenpo: function () {
         let select = document.getElementById('escolhaJokenpo');
         this.escolha = select.options[select.selectedIndex].value;
         this.imprimir();
-       
+
     },
-    imprimir:function(){
+    imprimir: function () {
         document.getElementById('escolhaPlayer').innerText = this.escolha;
     }
 }
 
 let computer = {
-    escolha:['Pedra', 'Papel', 'Tesoura'],       
-    buscaEscolhaJokenpo:function(){
-        this.escolha.sort();
+    escolha: [0, 1, 2],
+    escolhaComputer: "",
+    buscaEscolhaJokenpo: function () {
+
+        this.escolhaComputer = this.escolha[Math.floor(Math.random() * this.escolha.length)];
+
+        switch (this.escolhaComputer) {
+            case 0:
+                this.escolhaComputer = "Pedra";
+                break;
+            case 1:
+                this.escolhaComputer = "Papel";
+                break;
+            case 2:
+                this.escolhaComputer = "Tesoura";
+                break;
+        };
         this.imprimir();
-       
+
     },
-    imprimir:function(){
-        document.getElementById('escolhaComputer').innerText = this.escolha;
+    imprimir: function () {
+        document.getElementById('escolhaComputer').innerText = this.escolhaComputer;
     }
 }
 
 let jokenpo = {
-    jogar:function(){
+    jogar: function () {
         player.buscaEscolhaJokenpo();
         computer.buscaEscolhaJokenpo();
     }
