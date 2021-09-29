93ab6b3f7e71066c2debf532c38c31f6d2a54c29
Guilherme <guirafaellima@outlook.com>
2021-09-28 23:34:35 -0300

    Desenvolvimento Jokenpo

--- jokenpo.js
+++ jokenpo.js
@@ -1,3 +1,24 @@
+let player = {
+    escolhaPlayer:"",       
+    buscaEscolhaJokenpo:function(){
+        let select = document.getElementById('escolhaJokenpo');
+        this.escolhaPlayer = select.options[select.selectedIndex].value;
+       
+    },
+    imprimir:function(){
+        this.escescolhaPlayer = document.getElementById('escolhaPlayer').value;
+        
+    },
+
+
+}
+
+let computer = {
+
+}
+
 let jokenpo = {
 
 }
+
+console.log(this.escolhaPlayer);
