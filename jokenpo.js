5f69db5816407d25690daf7ecd5587863e171ff5
Guilherme Lima <glima@noovi.com.br>
2021-09-29 17:51:17 -0300

    Refatorando Código

--- jokenpo.js
+++ jokenpo.js
@@ -1,17 +1,23 @@
 let player = {
+    vitorias: 0,
     escolha: "",
     buscaEscolhaJokenpo: function () {
         let select = document.getElementById('escolhaJokenpo');
         this.escolha = select.options[select.selectedIndex].value;
         this.imprimir();
-
     },
     imprimir: function () {
         document.getElementById('escolhaPlayer').innerText = this.escolha;
+        document.getElementById('vitoriasPlayer1').innerText = this.vitorias;
+    },
+    ganhouRodada: function() {
+        this.vitorias += 1;
+        this.imprimir();
     }
 }
 
 let computer = {
+    vitorias: 0,
     escolha: [0, 1, 2],
     escolhaComputer: "",
     buscaEscolhaJokenpo: function () {
@@ -30,10 +36,55 @@ let computer = {
                 break;
         };
         this.imprimir();
-
     },
     imprimir: function () {
         document.getElementById('escolhaComputer').innerText = this.escolhaComputer;
+        document.getElementById('vitoriasComputer').innerText = this.vitorias;
+        
+    },
+    ganhouRodada: function() {
+        this.vitorias += 1;
+        this.imprimir();
+    }
+}
+
+let juiz = {
+    regrasJogo:function() {
+        
+        if(player.escolha == 'Papel' && computer.escolhaComputer == 'Papel'){
+            alert('Empate!');
+        }
+        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Pedra'){
+            alert('Empate!');
+        }
+        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Tesoura'){
+            alert('Empate!');
+        }
+        else if(player.escolha == 'Papel' && computer.escolhaComputer == 'Pedra'){
+            alert('Player Wins!!!');
+            player.ganhouRodada();
+        }
+        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Papel'){
+            alert('Player Wins!!!');
+            player.ganhouRodada();
+        }
+        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Tesoura'){
+            alert('Player Wins!!!');
+            player.ganhouRodada();
+        }
+       
+        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Papel'){
+            alert('Computer Wins!!!');
+            computer.ganhouRodada();
+        }
+        else if(player.escolha == 'Papel' && computer.escolhaComputer == 'Tesoura'){
+            alert('Computer Wins!!!');
+            computer.ganhouRodada();
+        }
+        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Pedra'){
+            alert('Player Wins!!!');
+            computer.ganhouRodada();
+        }
     }
 }
 
@@ -41,6 +92,16 @@ let jokenpo = {
     jogar: function () {
         player.buscaEscolhaJokenpo();
         computer.buscaEscolhaJokenpo();
+
+        juiz.regrasJogo();
+
+        
+
+       
+       
+
+
+
     }
 
 }
