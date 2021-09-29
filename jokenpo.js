40652a151af9a141fc591d04da39937bb43a3c87
Guilherme Lima <glima@noovi.com.br>
2021-09-29 10:59:35 -0300

    Definindo Escolha Jogadores

--- jokenpo.js
+++ jokenpo.js
@@ -1,24 +1,33 @@
 let player = {
-    escolhaPlayer:"",       
+    escolha:"",       
     buscaEscolhaJokenpo:function(){
         let select = document.getElementById('escolhaJokenpo');
-        this.escolhaPlayer = select.options[select.selectedIndex].value;
+        this.escolha = select.options[select.selectedIndex].value;
+        this.imprimir();
        
     },
     imprimir:function(){
-        this.escescolhaPlayer = document.getElementById('escolhaPlayer').value;
-        
-    },
-
-
+        document.getElementById('escolhaPlayer').innerText = this.escolha;
+    }
 }
 
 let computer = {
-
+    escolha:['Pedra', 'Papel', 'Tesoura'],       
+    buscaEscolhaJokenpo:function(){
+        this.escolha.sort();
+        this.imprimir();
+       
+    },
+    imprimir:function(){
+        document.getElementById('escolhaComputer').innerText = this.escolha;
+    }
 }
 
 let jokenpo = {
+    jogar:function(){
+        player.buscaEscolhaJokenpo();
+        computer.buscaEscolhaJokenpo();
+    }
 
 }
 
-console.log(this.escolhaPlayer);
