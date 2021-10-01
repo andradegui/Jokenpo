524755b71a09545b14e695d4e530ec55f0b40aa6
Guilherme Lima <glima@noovi.com.br>
2021-10-01 18:44:21 -0300

    Lista Partidas Funcionando

--- jokenpo.js
+++ jokenpo.js
@@ -1,16 +1,17 @@
 let player = {
     vitorias: 0,
-    escolha: "",
+    escolhaPlayer: "",
     buscaEscolhaJokenpo: function () {
         let select = document.getElementById('escolhaJokenpo');
-        this.escolha = select.options[select.selectedIndex].value;
+        this.escolhaPlayer = select.options[select.selectedIndex].value;
         this.imprimir();
+
     },
     imprimir: function () {
-        document.getElementById('escolhaPlayer').innerText = this.escolha;
+        document.getElementById('escolhaPlayer').innerText = this.escolhaPlayer;
         document.getElementById('vitoriasPlayer1').innerText = this.vitorias;
     },
-    ganhouRodada: function() {
+    ganhouRodada: function () {
         this.vitorias += 1;
         this.imprimir();
     }
@@ -18,90 +19,120 @@ let player = {
 
 let computer = {
     vitorias: 0,
-    escolha: [0, 1, 2],
+    escolha: ['Pedra', 'Papel', 'Tesoura'],
     escolhaComputer: "",
     buscaEscolhaJokenpo: function () {
 
-        this.escolhaComputer = this.escolha[Math.floor(Math.random() * this.escolha.length)];
+        this.escolhaComputer = this.escolha[Math.floor(Math.random() * 2)];
 
-        switch (this.escolhaComputer) {
-            case 0:
-                this.escolhaComputer = "Pedra";
-                break;
-            case 1:
-                this.escolhaComputer = "Papel";
-                break;
-            case 2:
-                this.escolhaComputer = "Tesoura";
-                break;
-        };
+
         this.imprimir();
+
+
     },
     imprimir: function () {
         document.getElementById('escolhaComputer').innerText = this.escolhaComputer;
         document.getElementById('vitoriasComputer').innerText = this.vitorias;
-        
+
     },
-    ganhouRodada: function() {
+    ganhouRodada: function () {
         this.vitorias += 1;
         this.imprimir();
     }
 }
 
 let juiz = {
-    regrasJogo:function() {
-        
-        if(player.escolha == 'Papel' && computer.escolhaComputer == 'Papel'){
-            alert('Empate!');
+    ganhadorPartida: "",
+    regrasJogo: function () {
+
+        //EMPATES
+        if (player.escolhaPlayer == 'Papel' && computer.escolhaComputer == 'Papel') {
+            this.ganhadorPartida = "Empate";
         }
-        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Pedra'){
-            alert('Empate!');
+        else if (player.escolhaPlayer == 'Pedra' && computer.escolhaComputer == 'Pedra') {
+            this.ganhadorPartida = "Empate";
         }
-        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Tesoura'){
-            alert('Empate!');
+        else if (player.escolhaPlayer == 'Tesoura' && computer.escolhaComputer == 'Tesoura') {
+            this.ganhadorPartida = "Empate";
         }
-        else if(player.escolha == 'Papel' && computer.escolhaComputer == 'Pedra'){
-            alert('Player Wins!!!');
+        //PLAYER WINS
+        else if (player.escolhaPlayer == 'Papel' && computer.escolhaComputer == 'Pedra') {
             player.ganhouRodada();
+            this.ganhadorPartida = "Player";
         }
-        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Papel'){
-            alert('Player Wins!!!');
+        else if (player.escolhaPlayer == 'Tesoura' && computer.escolhaComputer == 'Papel') {
+
             player.ganhouRodada();
+            this.ganhadorPartida = "Player";
         }
-        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Tesoura'){
-            alert('Player Wins!!!');
+        else if (player.escolhaPlayer == 'Pedra' && computer.escolhaComputer == 'Tesoura') {
+
             player.ganhouRodada();
+            this.ganhadorPartida = "Player";
         }
-       
-        else if(player.escolha == 'Pedra' && computer.escolhaComputer == 'Papel'){
-            alert('Computer Wins!!!');
+        //COMPUTER WINS
+        else if (player.escolhaPlayer == 'Pedra' && computer.escolhaComputer == 'Papel') {
+
+            computer.ganhouRodada();
+            this.ganhadorPartida = "Computer";
+        }
+        else if (player.escolhaPlayer == 'Papel' && computer.escolhaComputer == 'Tesoura') {
+
+            this.ganhadorPartida = "Computer";
             computer.ganhouRodada();
         }
-        else if(player.escolha == 'Papel' && computer.escolhaComputer == 'Tesoura'){
-            alert('Computer Wins!!!');
+        else if (player.escolhaPlayer == 'Tesoura' && computer.escolhaComputer == 'Pedra') {
+
+            this.ganhadorPartida = "Computer";
             computer.ganhouRodada();
         }
-        else if(player.escolha == 'Tesoura' && computer.escolhaComputer == 'Pedra'){
-            alert('Player Wins!!!');
-            computer.ganhouRodada();
-        }
+
+
+        this.imprimir();
+    },
+
+    imprimir: function () {
+        document.getElementById('vencedor').innerText = this.ganhadorPartida;
     }
 }
 
 let jokenpo = {
+    historicoPartidas: [],
     jogar: function () {
+
         player.buscaEscolhaJokenpo();
         computer.buscaEscolhaJokenpo();
 
         juiz.regrasJogo();
 
+        let saveChose = {
+            escolhaComputer: computer.escolhaComputer,
+            escolhaPlayer: player.escolhaPlayer,
+            ganhadorPartida: juiz.ganhadorPartida,
+            NumeroPartida: this.historicoPartidas.length
+        }
+        jokenpo.historicoPartidas.push(saveChose);
+
+        let table = document.getElementById('tabela').getElementsByTagName('tbody')[0];
         
 
+        for (let i = 0; i < this.historicoPartidas.length; i++) {
+
+            table.innerHTML += this.criaLinha(this.historicoPartidas[i]);
+            
+            
+        }
+
        
-       
+    },
 
-
-
+    criaLinha: function (objeto) {
+        return `<tr>
+                    <td>${objeto.NumeroPartida}</td>
+                    <td>${objeto.escolhaPlayer}</td>
+                    <td>${objeto.escolhaComputer}</td>    
+                    <td>${objeto.ganhadorPartida}</td>    
+                </tr>`;
     }
 
 }
